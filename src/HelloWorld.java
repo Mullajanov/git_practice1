0674a8d450397dac7c4bcec2b30aae3716a53e8f
Sherzod <seanmullajanov@gmail.com>
2022-01-11 08:15:08 -0800

    deleted power package and all txt files than ignored all *txt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,15 @@
 public class HelloWorld {
 
-     // Doing some work here ( you can type anything you want)
+    // Doing some work here ( you can type anything you want)
 
     // Doing awesome work 2 !
 
     // Doing awesome work 3 !
     // Awesome work 3 is done and ready to be committed
     // and ready to be pushed to remote repository
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 
 }
